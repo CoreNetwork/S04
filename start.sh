7054f423ec3d53fa99bf165f3a078dacd2f3f0f1
riddle <piotr@riddle.pl>
2015-04-16 19:35:12 +0200

    Forgot to do a thing

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-java -Dfile.encoding=UTF8 -Djava.awt.headless=true -Dlog4j.configurationFile=log4j2.xml -Xmx4096m -server -jar spigot-1.8-dev.jar nogui
+java -Dfile.encoding=UTF8 -Djava.awt.headless=true -Dlog4j.configurationFile=log4j2.xml -Xmx4096m -server -jar spigot-1.8.3-dev.jar nogui
